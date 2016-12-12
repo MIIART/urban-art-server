aeb95477ef55c52d2d2a3c46c0cc300f30975094
sylvain Rojas <sylvain.rojas@gmail.com>
2016-12-12 14:00:03 +0100

    commit

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ const bodyParser = require('body-parser');
 
 const root = __dirname.replace("urban-art-server","urban-art-ionic")+"/www"
 
-// const api = require('./modules/api').api;
+const api = require('./modules/api').api;
 
 app
 	.use(express.static(root))
 	.use(bodyParser.urlencoded({extended: true}))
 	.use(bodyParser.json())
 
+app
+	.get('/works', api.get)
+	.post('/works', api.add)
+	.delete('/:id', api.delete )
 
 server.listen(port, ()=>{
 	console.log("Listnening on port " + port)
