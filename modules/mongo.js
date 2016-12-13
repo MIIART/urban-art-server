ec81eacf6dd0c518340bc7a86c0d71e1042661f6
sylvain Rojas <sylvain.rojas@gmail.com>
2016-12-13 13:20:20 +0100

    insert works as comments

--- modules/mongo.js
+++ modules/mongo.js
@@ -14,6 +14,30 @@ const workSchema = mongoose.Schema({
     artists: [String]
 });
 
+/*
+db.getCollection('works').insert({
+        photos: ["graf-1.jpg"],
+        title : "Deep sea blue",
+        datePosted: new Date(),
+        address: "place des augustins, Genève",
+        pos:[6.144571, 46.191814],
+        description: "A beautiful work",
+        idCat: 0,
+        artists: ["wertucuhkjhjfa"],
+    })
+
+db.getCollection('works').insert({
+        photos: ["graf-2.jpg"],
+        title : "Bienvenu au coin",
+        datePosted: new Date(),
+        address: "Place de Bel-Air, Genève",
+        pos:[6.143171, 46.204366],
+        description: "Another beautiful work",
+        idCat: 0,
+        artists: ["wertucuhkjhjfa"],
+    })
+*/
+
 const Work = mongoose.model('work', workSchema); // work est un modèle Mongoose.
 												 // 'work' indique à Mongoose d'utiliser la collection 'works'
 mongoose.connect('mongodb://localhost/urban', (err) => {
