b5eb9074ddbc2bf340059c31b3598bb78a234f13
sylvain Rojas <sylvain.rojas@gmail.com>
2016-12-22 10:46:25 +0100

    process.env.PORT || 8000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const server = require('http').createServer(app);
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const bodyParser = require('body-parser');
 
